8141d76c32780332ed48509d15c75f93541f5c74
mohamed <mohamed.bd.business@gmail.com>
2025-12-12 00:55:49 +0100

    change kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
-        $schedule->command('orders:update-timers')->daily();
+        $schedule->command('orders:update-timers')->hourly();
     }
 
     /**
