a7c7ec868618816e21095e857bfc3cf5a9e6f77e
mohamedbayad <mohamed.bd.business@gmail.com>
2026-04-05 11:31:05 -0700

    change kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('orders:update-timers')->hourly();
+        $schedule->command('orders:update-timers')->everyMinute();
     }
 
     /**
